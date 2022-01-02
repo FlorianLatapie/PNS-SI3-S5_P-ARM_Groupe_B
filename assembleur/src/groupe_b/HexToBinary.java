ee359fb18a9dd54e6aa2a8ff59db175a13036382
Florian LATAPIE <70631622+FlorianLatapie@users.noreply.github.com>
2022-01-02 16:56:11 +0100

    correction alu

--- assembleur/src/groupe_b/HexToBinary.java
+++ assembleur/src/groupe_b/HexToBinary.java
@@ -5,7 +5,7 @@ import static groupe_b.Fonctions.padLeftZeros;
 
 public class HexToBinary {
     public static void main(String[] args) {
-        String entree = "b099 b0ff b0f1 2000 9003 2001 9002 2002 9001 2003 9000 e7fe 980e 9903 4288 d104 e7fe 980c 990d 1840 900a e01f 980e 9902 4288 d104 e7fe 980c 990d 1a40 900a e014 980e 9901 4288 d104 e7fe 980c 990d 4341 910a e009 980e 9900 4288 d104 e7fe 980c 990d 4088 900a e7fe e7fe e7fe e7fe e7d2 ";
+        String entree = "2000 2101 2214 4288 d4ff e7ff 4252 428a dbff e000 2032 e7f4 1883 ";
         String[] hex = entree.split(" ");
 
         for (int i = 0; i < hex.length; i++) {
